03f61bdde6b2b723438a5d51d2fde0e997da6870
Gerson <88354800+moskvat2@users.noreply.github.com>
2024-01-20 13:44:08 -0300

    Update installDocker.sh

--- installDocker.sh
+++ installDocker.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 yum remove -y docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-engine
+rm -f /usr/bin/docker-compose
 yum install -y yum-utils
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 yum-config-manager --enable docker-ce-nightly
